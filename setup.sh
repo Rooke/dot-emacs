dd57fd38b68b8e2a3d3586531d6dbe06532cac68
Michael Rooke <michaelr@sortable.com>
2019-01-21 14:19:15 -0500

    Add some bashrc exports and ensime settings

--- setup.sh
+++ setup.sh
@@ -8,3 +8,23 @@ wget -O ~/.emacs.d/git-blame.el 'https://raw.github.com/git/git/master/contrib/e
 sudo apt-get install cscope-el ecb
 mv ./emacs.d/init.el ~/.emacs.d/init.el
 mv -r ./emacs.d/customizations/ ~/.emacs.d/customizations/
+
+# Handy git
+alias gcm='git checkout master'
+alias gpm='git pull sortable master'
+alias gd='git difftool --dir'
+alias gsort='git branch --sort=committerdate'
+alias gpp='git push --force origin HEAD'
+export GPG_TTY=$(tty)
+
+# Sortable-specific
+alias restore_sortable='sudo $HOME/dev/tng/data/bin/restore_sortable -d data -r /mnt/d2/redis/redis-dev.rdb -l /mnt/d2/elasticsearch'
+alias lein='~/dev/tng/lein'
+alias fj='~/dev/tng/fj'
+alias sbt='~/dev/tng/sbt'
+
+# Go
+export PATH=$PATH:~/.nix-profile/bin:/usr/local/go/bin
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=~/go
+export PATH=$PATH:$GOPATH/bin
